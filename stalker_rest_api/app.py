a721f998d71e98216646b88b16dd19b604529f5d
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2017-10-02 16:35:57 +0200

    Use marshmallow for serialization (nice!)

--- stalker_rest_api/app.py
+++ stalker_rest_api/app.py
@@ -3,13 +3,14 @@ import json
 
 
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 from flask_jwt_extended import (
     JWTManager,
     jwt_required,
     create_access_token,
     get_jwt_identity,
 )
+from marshmallow import Schema, fields, post_load
 from sqlalchemy import or_
 
 
@@ -34,19 +35,16 @@ def connect_to_stalker():
     db.setup(settings=database_engine_settings)
 
 
-def format_user(user):
-    """Format a user for json"""
-    return {"id": user.id,
-            "name": user.name,
-            "login": user.login,
-            "email": user.email}
+class UserSchema(Schema):
+    user_id = fields.Integer()
+    name = fields.String(required=True)
+    login = fields.String(required=True)
+    email = fields.Email(required=True)
+    password = fields.String(required=True)
 
-
-def format_project(project):
-    """Format a project for json"""
-    return {"id": project.id,
-            "name": project.name,
-            "code": project.code}
+    @post_load
+    def make_user(self, data):
+        return User(**data)
 
 
 class Login(Resource):
@@ -72,79 +70,41 @@ class ApiUser(Resource):
         if not user:
             return {'user': None}, 404
         if user.id == identity:
-            return format_user(user)
+            data, errors = UserSchema(exclude=('password',)).dump(user)
+            if errors:
+                return errors
+            return data, 200
         else:
             return {'message': 'User is not authorized to view information of other users'}, 401
 
     def post(self, login):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            "name",
-            type=str,
-            required=True,
-            help="A new user should have a name",
-        )
-        parser.add_argument(
-            "email",
-            type=str,
-            required=True,
-            help="A new user should have an email",
-        )
-        parser.add_argument(
-            "password",
-            type=str,
-            required=True,
-            help="A new user should have a password",
-        )
-        data = parser.parse_args()
+        data = request.get_json()
+        user, errors = UserSchema().load(data)
+        if errors:
+            return errors
         if User.query.filter(or_(User.login==login, User.email==data['email'])).first():
             return {'message': 'A user with this login or email already exists'}, 400
-        user = User(
-            name=data['name'],
-            email=data['email'],
-            login=login,
-            password=data['password'],
-        )
         DBSession.save(user)
-        return format_user(user), 201
+        user_data, errors = UserSchema(exclude=('password',)).dump(user)
+        if errors:
+            return errors
+        return user_data, 201
 
     def put(self, login):
-        parser = reqparse.RequestParser()
-        data = parser.parse_args()
+        data = request.get_json()
         if not User.query.filter_by(login=login).first():
             if data.get('email') and User.query.filter_by(email=data['email']).first():
                 return {'message': 'A user with this email already exists'}, 400
-            parser = reqparse.RequestParser()
-            parser.add_argument(
-                "name",
-                type=str,
-                required=True,
-                help="A new user should have a name",
-            )
-            parser.add_argument(
-                "email",
-                type=str,
-                required=True,
-                help="A new user should have an email",
-            )
-            parser.add_argument(
-                "password",
-                type=str,
-                required=True,
-                help="A new user should have a password",
-            )
-            data = parser.parse_args()
-            user = User(
-                name=data['name'],
-                email=data['email'],
-                login=login,
-                password=data['password'],
-            )
+            schema = UserSchema()
+            result = schema.load(data)
+            if result.errors:
+                return result.errors
+            user = result.data
         else:
             if data.get('email') and User.query.filter(User.email==data['email'], User.login!=login).first():
                 return {'message': 'A user with this email already exists (email not unique)'}, 400
             if data.get('login') and data['login'] != login and User.query.filter_by(login=data['login']).first():
-                return {'message': 'A user with login \'{}\' already exists'.format(data['login'])}, 400
+                return {'message': 'A user with this login already exists'}, 400
             user = User.query.filter_by(login=login).first()
             if data.get('login'):
                 user.login = data['login']
@@ -155,7 +115,8 @@ class ApiUser(Resource):
             if data.get('password'):
                 user.password = data['password']
         DBSession.save(user)
-        return format_user(user), 201
+        data, errors = UserSchema(exclude=('password',)).dump(user)
+        return data, 201
 
     def delete(self, login):
         user = User.query.filter_by(login=login).first()
@@ -163,14 +124,20 @@ class ApiUser(Resource):
             return {'user': None}, 404
         DBSession.delete(user)
         DBSession.commit()
-        return {'message': 'Deleted user \'{}\''.format(login)}
+        data, errors = UserSchema(exclude=('password',)).dump(user)
+        if errors:
+            return errors
+        return {'message': 'Deleted user', 'user': data}
 
 
 class ApiUsers(Resource):
     def get(self):
-        users = [format_user(user) for user in User.query.all()]
+        users = [user for user in User.query.all()]
         if users:
-            return users
+            data, errors = UserSchema(exclude=('password',), many=True).dump(users)
+            if errors:
+                return errors
+            return data
         return {'users': None}, 404
 
 
