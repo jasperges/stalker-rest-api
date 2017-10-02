d3169c32b68ae317de98304f92a814db414d0975
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2017-10-02 14:28:13 +0200

    Added put method for user

--- stalker_rest_api/app.py
+++ stalker_rest_api/app.py
@@ -89,6 +89,36 @@ class ApiUser(Resource):
         DBSession.save(user)
         return format_user(user), 201
 
+    def put(self, login):
+        data = request.get_json()
+        if not User.query.filter_by(login=login).first():
+            if data.get('email') and User.query.filter_by(email=data['email']).first():
+                return {'message': 'A user with this email already exists'}, 400
+            if not data.get('name') or not data.get('email') or not data.get('password'):
+                return {'message': 'For new users name, email and password are required'}, 400
+            user = User(
+                name=data['name'],
+                email=data['email'],
+                login=login,
+                password=data['password'],
+            )
+        else:
+            if data.get('email') and User.query.filter(User.email==data['email'], User.login!=login).first():
+                return {'message': 'A user with this email already exists (email not unique)'}, 400
+            if data.get('login') and data['login'] != login and User.query.filter_by(login=data['login']).first():
+                return {'message': 'A user with login \'{}\' already exists'.format(data['login'])}, 400
+            user = User.query.filter_by(login=login).first()
+            if data.get('login'):
+                user.login = data['login']
+            if data.get('name'):
+                user.name = data['name']
+            if data.get('email'):
+                user.email = data['email']
+            if data.get('password'):
+                user.password = data['password']
+        DBSession.save(user)
+        return format_user(user), 201
+
     def delete(self, login):
         user = User.query.filter_by(login=login).first()
         if not user:
